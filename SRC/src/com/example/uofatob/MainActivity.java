bbd70b52fca6561d4c2bdc6c29b8e353cbc5d135
CptJamesCook <mrniceguyjames@gmail.com>
2016-01-31 00:58:32 -0800

    Started Main Activity Backend

--- SRC/src/com/example/uofatob/MainActivity.java
+++ SRC/src/com/example/uofatob/MainActivity.java
@@ -4,13 +4,43 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+    private String[] spinnerItems;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        final Button goButton = (Button) findViewById(R.id.button);
+        final EditText startText = (EditText) findViewById(R.id.editText);
+        final EditText destText = (EditText) findViewById(R.id.editText2);
+        final Spinner startSpin = (Spinner) findViewById(R.id.StartPoint);
+        final Spinner destSpin = (Spinner) findViewById(R.id.EndPoint);
+        this.spinnerItems = new String[]{
+                "ETLC","CAB"
+        };
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerItems);
+        startSpin.setAdapter(spinnerAdapter);
+        destSpin.setAdapter(spinnerAdapter);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String startRoom = startText.getText().toString();
+                String destRoom = destText.getText().toString();
+                //Need to have queries to firebase
+                
+
+            }
+
+        });
 	}
 
 	@Override
@@ -31,4 +61,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
