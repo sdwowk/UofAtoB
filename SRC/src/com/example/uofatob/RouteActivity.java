dac0a9993c338055d975de22d6b0e183c3f330c5
CptJamesCook <mrniceguyjames@gmail.com>
2016-01-30 20:38:49 -0800

    adjustments to route activity

---
+++ SRC/src/com/example/uofatob/RouteActivity.java
@@ -0,0 +1,14 @@
+package com.example.uofatob;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by James Cook on 30/01/2016.
+ */
+public class RouteActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
